8ae3b97228e5a370effc8a1fffe1a746b6ea658b
haivuw <haivuw@gmail.com>
2023-01-18 15:51:43 +0700

    feat: proxy api to external one

--- apps/infra/src/test-stack.ts
+++ apps/infra/src/test-stack.ts
@@ -1,14 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { aws_lambda_nodejs, Duration } from 'aws-cdk-lib';
+import { aws_lambda_nodejs, CfnOutput, Duration } from 'aws-cdk-lib';
+import { FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
 
 export class TestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new aws_lambda_nodejs.NodejsFunction(this, 'TRPCHandler', {
+    const trpc = new aws_lambda_nodejs.NodejsFunction(this, 'TRPCHandler', {
       entry: require.resolve('./trpc'),
       timeout: Duration.minutes(1),
+    });
+
+    const url = trpc.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+
+    new CfnOutput(this, 'TRPCUrl', {
+      value: url.url,
     });
   }
 }
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,20 @@
+const { TRPC_URL } = process.env;
+
+if (!TRPC_URL) {
+  throw new Error('TRPC_URL env var is missing');
+}
+
 module.exports = {
   reactStrictMode: true,
   experimental: {
-    transpilePackages: ["ui"],
+    transpilePackages: ['ui'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${TRPC_URL}/:path*`, // Proxy to Backend
+      },
+    ];
   },
 };
